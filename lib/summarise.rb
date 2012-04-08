839a807b7a91406b58cc244dad9bf2e05f3d186b
Oliver Hookins <ohookins@gmail.com>
2012-04-08 12:21:25 +0200

    Fix summarise to work against mysql2 gem.

--- lib/summarise.rb
+++ lib/summarise.rb
@@ -103,37 +103,34 @@ class Summarise
 
   # adds an ip (host) entry to the rails database
   def self.add_ip(params = {:dconn => nil, :ip => nil, :loglevel => 0})
-    insert_id = nil
-    params[:dconn].class == Mysql or raise "#{this_method_name} was not passed a valid MySQL connection."
+    params[:dconn].class == Mysql2::Client or raise "#{this_method_name} was not passed a valid MySQL connection."
     IPAddr.new(params[:ip]) or raise "#{this_method_name} was not passed a valid IP address."
 
     # check the IP is not already there
     stmt = "SELECT id FROM hosts WHERE ip = \'#{params[:ip]}\'"
     log(2, stmt, params[:loglevel])
     res = params[:dconn].query(stmt)
-    if res and res.num_rows() >= 1 then
-      if res.num_rows() > 1 then
+    if res and res.count() >= 1 then
+      if res.count() > 1 then
         raise "#{params[:ip]} found multiple times in the hosts table, this should not happen."
       end
-      insert_id = res.fetch_row()
-      log(2, "#{params[:ip]} found at ID #{insert_id} in hosts table.", params[:loglevel])
-      res.free
+      log(2, "#{params[:ip]} found at ID #{params[:dconn].last_id()} in hosts table.", params[:loglevel])
 
     # didn't find the IP address in the table, so add it
     else
       begin
         stmt = "INSERT INTO hosts (id, ip, created_at, updated_at) VALUES (NULL, \'#{params[:ip]}\', NOW(), NOW())"
         log(2, stmt, params[:loglevel])
+        params[:dconn].query('START TRANSACTION')
         params[:dconn].query(stmt)
-        insert_id = params[:dconn].insert_id
-        log(2, "#{params[:ip]} inserted at ID #{insert_id}", params[:loglevel])
-        params[:dconn].commit
+        log(2, "#{params[:ip]} inserted at ID #{params[:dconn].last_id}", params[:loglevel])
+        params[:dconn].query('COMMIT')
       rescue => detail
-        params[:dconn].rollback
+        params[:dconn].query('ROLLBACK')
         raise "insertion of #{params[:ip]} failed and was rolled back in #{this_method_name} => #{detail}"
       end
     end
-    return insert_id
+    return params[:dconn].last_id
   end
 
   # add usage for local IPs for a date to the rails database
@@ -144,14 +141,14 @@ class Summarise
 
     stmt = "SELECT ip_src,ip_dst,bytes FROM acct WHERE DATE(stamp_inserted) = \'#{params[:date]}\'"
     log(2, stmt, params[:loglevel])
-    res = params[:sconn].query(stmt)
-    res and log(2, "number of rows: #{res.num_rows()}", params[:loglevel])
+    res = params[:sconn].query(stmt, :stream => true)
+    res and log(2, "number of rows: #{res.count()}", params[:loglevel])
 
     # Process each row in the result set and tally the bytes for each valid IP
     source_matched = 0 # purely diagnostic tracking
     dest_matched = 0 # ditto
     bytesbyip = {} # keep a hash of our matched IPs and their byte counters like {IP => [inbytes, outbytes]}
-    res and res.each_hash do |r|
+    res and res.each do |r|
       if matches_subnets?(r['ip_src']) and not matches_subnets?(r['ip_dst']) then
         source_matched += 1
         bytesbyip[r['ip_src']] or bytesbyip[r['ip_src']] = [0,0] # create a record in the hash if missing
@@ -162,7 +159,6 @@ class Summarise
         bytesbyip[r['ip_dst']][0] += r['bytes'].to_i
       end
     end
-    res and res.free
     log(1, "source_matched: #{source_matched} for #{params[:date]}", params[:loglevel])
     log(1, "dest_matched: #{dest_matched} for #{params[:date]}", params[:loglevel])
     log(2, bytesbyip.each { |k,v| puts "#{k}: #{v[0]} in, #{v[1]} out" }, params[:loglevel])
@@ -173,11 +169,11 @@ class Summarise
       log(2, stmt, params[:loglevel])
       res = params[:dconn].query(stmt)
       id = nil
-      unless res and res.num_rows() == 1 then
+      unless res and res.count() == 1 then
         log(0, "#{this_method_name} wasn't able to find #{k} in the database, but it has usage data.", params[:loglevel])
         next
       else
-        id = res.fetch_row()[0]
+        id = res.first['id']
       end
 
       # Add the record now, convert numbers from bytes to MB in fixed decimal
@@ -185,10 +181,11 @@ class Summarise
         inMB = (BigDecimal.new(v[0].to_s)/(1024**2)).round(2).to_f
         outMB = (BigDecimal.new(v[1].to_s)/(1024**2)).round(2).to_f
         stmt = "INSERT INTO usage_entries VALUES (NULL, #{id}, \'#{inMB}\', \'#{outMB}\', \'#{params[:date]}\', NOW(), NOW())"
+        params[:dconn].query('START TRANSACTION')
         params[:dconn].query(stmt)
-        params[:dconn].commit
+        params[:dconn].query('COMMIT')
       rescue => detail
-        params[:dconn].rollback
+        params[:dconn].query('ROLLBACK')
         raise "Error while inserting usage for #{params[:date]} in #{this_method_name} => #{detail}"
       end
     end
@@ -203,20 +200,27 @@ class Summarise
 
     begin
       # establish source database connection (raw pmacct database)
-      sconn = Mysql::connect(@@sourcehost, @@sourceuser, @@sourcepass, @@sourcedb)
+      sconn = Mysql2::Client.new({:host     => @@sourcehost,
+                                  :username => @@sourceuser,
+                                  :password => @@sourcepass,
+                                  :database => @@sourcedb})
       unless sconn
         fail("Couldn't connect to database #{@@sourcedb} with #{@@sourceuser}@#{@@sourcehost} with given password.")
       end
-      sconn.autocommit(0)
+      sconn.query('SET AUTOCOMMIT = 0')
 
       # establish destination database connection
-      dconn = Mysql::connect(@@desthost, @@destuser, @@destpass, @@destdb)
+      dconn = Mysql2::Client.new({:host     => @@desthost,
+                                  :username => @@destuser,
+                                  :password => @@destpass,
+                                  :database => @@destdb})
       unless dconn
         fail("Couldn't connect to database #{@@destdb} with #{@@destuser}@#{@@desthost} with given password.")
       end
-      dconn.autocommit(0)
+      dconn.query('SET AUTOCOMMIT = 0')
     rescue
       sconn && sconn.close
+      raise
     end
 
     begin
@@ -225,25 +229,24 @@ class Summarise
       stmt = "SELECT COALESCE((SELECT MAX(date) FROM usage_entries), \'#{STARTDATE}\') AS date"
       log(2, stmt, loglevel)
       res = dconn.query(stmt)
-      if res and res.num_rows() == 1 then
-        import_date = res.fetch_row()
+      if res and res.count() == 1 then
+        import_date = res.first['date']
         log(1, "last import date: #{import_date}", loglevel)
-        res.free
       else
         fail('Unable to determine last usage import date, or use a default value.')
       end
-
       # Determine list of days we have to process.
       # We don't want to import today's stats as we won't have the complete day's stats.
       stmt = "SELECT DISTINCT(DATE(stamp_inserted)) AS date FROM acct WHERE DATE(stamp_inserted) > \'#{import_date}\' AND DATE(stamp_inserted) < DATE(NOW()) ORDER BY date"
       log(2, stmt, loglevel)
       res = sconn.query(stmt)
-      if res and res.num_rows() >= 1 then
-        res.each do |r|
-          r_date = r[0]
-          r_date.class == String and import_list.push(r_date) and log(1, "need to import: #{r_date}", loglevel)
+      if res.count() >= 1 then
+        res.each do |row|
+          if row['date'].class == Date
+            import_list.push(row['date'])
+            log(1, "need to import: #{row['date']}", loglevel)
+          end
         end
-        res.free
       else
         log(1, "Nothing to import", loglevel)
         return(0)
@@ -258,17 +261,16 @@ class Summarise
         # MySQL can't do the calculations natively, sadly.
         stmt = "SELECT DISTINCT(ip_src) AS ip FROM acct WHERE DATE(stamp_inserted) = \'#{d}\' UNION DISTINCT SELECT DISTINCT(ip_dst) AS ip FROM acct WHERE DATE(stamp_inserted) = \'#{d}\'"
         log(2, stmt, loglevel)
-        res = sconn.query(stmt)
+        res = sconn.query(stmt, :stream => true)
 
         # Pick out valid IP addresses and add them to the "hosts" rails table
         valid_ips = []
         res and res.each do |r|
-          if matches_subnets?(r[0]) then
-            log(1, "Found valid IP address: #{r[0]}", loglevel)
-            add_ip({:dconn => dconn, :ip => r[0], :loglevel => loglevel})
+          if matches_subnets?(r['ip']) then
+            log(1, "Found valid IP address: #{r['ip']}", loglevel)
+            add_ip({:dconn => dconn, :ip => r['ip'], :loglevel => loglevel})
           end
         end
-        res and res.free
 
         # Summarise traffic 
         add_usage({:sconn => sconn, :dconn => dconn, :date => d, :loglevel => loglevel})
